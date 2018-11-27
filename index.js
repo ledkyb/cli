019fa3f42231a11e2038e662d9d9593d1a7a941d
lloan <lloanalas@outlook.com>
2018-11-27 11:24:25 -0800

    initial testing

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const program = require('commander'),
+	{prompt} = require('inquirer'),
+	app = require('./build')
+	shell = require('shelljs');
+
+
+program
+	.version('1.0.0')
+	.description('A react site generator CLI.');
+
+program
+	.command('test')
+	.description('testing application')
+	.action(() => {
+		prompt({
+			type: 'input',
+			name: 'name',
+			message: 'project name'
+		}).then(answer => {
+			const name = answer.name;
+			app.base(name);  
+			app.install(name);
+
+		})
+	})
+
+program
+	.parse(process.argv);
