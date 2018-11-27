918877876aa7d3dfdb9a9ffdd8df8f5372b8d7df
lloan <lloanalas@outlook.com>
2018-11-27 11:24:10 -0800

    logic

---
+++ build.js
@@ -0,0 +1,22 @@
+export default class App {
+	base(path){
+		if (shell){
+			shell.exec(`create-react-app ${path}`);
+			shell.echo('CRA base has been loaded');
+		}
+	}
+
+	navigate(path){
+		if(shell){
+			shell.cd(`${path}`);
+		}
+	}
+
+	install(path){
+		if(shell){
+			this.navigate(path);
+			shell.exec(`pwd`);
+			//shell.exec(`npm install --save colors`);
+		}
+	}
+}
