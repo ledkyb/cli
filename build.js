57ad912f616ad9b581d83b20053a29b44432fa8b
lloan <lloanalas@outlook.com>
2018-11-27 16:35:49 -0800

    working

--- build.js
+++ build.js
@@ -1,26 +1,71 @@
+const chalk = require('chalk'),
+	shell = require('shelljs'),
+	ora = require('ora');
+
 class myApp {
-	base(path){
-		if (shell){
-			shell.exec(`create-react-app ${path}`);
-			shell.echo('CRA base has been loaded');
+
+	welcome() {
+		if (shell) {
+			shell.echo(chalk.keyword('orange').bold('\nCreate-react-site CLI\n'))
 		}
 	}
 
-	navigate(path){
-		if(shell){
-			shell.cd(`${path}`);
+	initMessage() {
+		shell.echo(chalk.keyword('orange')('\nYour project will begin building now...\n\n'))
+	}
+
+	init(path) {
+
+		// while a spinner is running, if you echo, you will get a duplicate without a check mark
+
+		if (shell) {
+
+			new Promise((resolve, reject) => {
+				let spinner = ora('\nLoading CRA...').start();
+				shell.exec(`create-react-app ${path}`, {
+					async: true,
+					silent: true
+				}, () => {
+					spinner.succeed('CRA loaded as base!');
+					resolve(true);
+				});
+
+			}).then(response => {
+				this.install(path);
+				//spinner.succeed();  // call in the next promise once its resolved
+			});
 		}
 	}
 
-	install(path){
-		if(shell){
-			this.navigate(path);
-			shell.exec(`pwd`);
-			//shell.exec(`npm install --save colors`);
+	install(path) {
+		if (shell) {
+
+			new Promise((resolve, reject) => {
+				let spinner = ora('\nInstalling additional packages...').start();
+
+				shell.cd(path);
+				shell.exec(`yarn add colors`, {
+					async: true,
+					silent: true
+				}, () => {
+					spinner.succeed('Additional packages installed!');
+					resolve(true);
+				});
+
+			}).then(response => {
+				//spinner.succeed();  // call in the next promise once its resolved
+			});
 		}
+	}
+
+	success(){
+		shell.echo(chalk.keyword('green').bold('\nProject created successfully!\n'))
+		shell.echo(chalk.keyword('green')(`\nYou can navigate to ${path}\\ and `) + chalk.keyword('orange')(`yarn start\n`))
 	}
 }
 
 const app = new myApp();
 
-module.exports = { app }
+module.exports = {
+	app
+}
--- index.js
+++ index.js
@@ -2,26 +2,26 @@
 
 const program = require('commander'),
 	{prompt} = require('inquirer'),
-	{app} = require('./build')
-	shell = require('shelljs');
-
+	{app} = require('./build');
 
 program
 	.version('1.0.0')
 	.description('A react site generator CLI.');
 
 program
-	.command('test')
+	.command('start')
 	.description('testing application')
 	.action(() => {
+
+		app.welcome();
+
 		prompt({
 			type: 'input',
 			name: 'name',
-			message: 'project name'
-		}).then(answer => {
-			const name = answer.name;
-			app.base(name);  
-			app.install(name); 
+			message: 'Project name: '
+		}).then(res => { 
+			app.initMessage();
+			app.init(res.name);
 		})
 	})
 
